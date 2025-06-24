7841ed253d6512c4eaa6679f0ffa6c9a7c24b10d
msoftools <msoftools@gmail.com>
2025-06-24 08:23:00 -0600

    test

--- Local/main.tf
+++ Local/main.tf
@@ -1,8 +1,25 @@
-resource "" "name" {
-  #esto es un test
-  #este es otro test 
-  #estes es un 3 test
-#este es un 4 test
-#5to tests
-  
+terraform {
+  required_version = "~>1.9.0"  
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
 }
+
+provider "local" {
+  # Configuration options
+}
+
+resource "local_file" "local-file-creation" {
+   file_permission = "0400"
+   filename = "learning.txt"
+   content = "Welcome to the terraform platform "
+}
+
+resource "local_file" "bar" {
+  filename = "bar.txt"
+  content = "Hello Terraform re:invent 2024"
+  file_permission = "0400"
+} 

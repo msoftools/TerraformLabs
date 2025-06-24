2008ac9b38cb589106f048653574b338e43c55a5
msoftools <msoftools@gmail.com>
2025-06-24 16:10:39 -0600

    t2

--- Local/randomExample/main.tf
+++ Local/randomExample/main.tf
@@ -6,6 +6,18 @@ resource "random_pet" "name" {
   length=3
 }
 
+
+resource "local_file" "name" {
+   file_permission = "0400"
+   filename = "learning.txt"
+   content = "Welcome to the terraform platform "
+}
+
+
 output "random_pet_name" {
   value = random_pet.name.id
+}
+
+output "random_pet_name" {
+  value = local_file.name.id
 }

96f675415ac62fb95d2304cc3132c9f36ead870d
msoftools <msoftools@gmail.com>
2025-06-24 08:23:55 -0600

    test

--- Local/main.tf
+++ Local/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = "~>1.9.0"  
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.2"
-    }
-  }
-}
-
 provider "local" {
   # Configuration options
 }

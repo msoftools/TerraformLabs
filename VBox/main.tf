0c313fc15465310d766e4e298da3a74a792f724d
msoftools <msoftools@gmail.com>
2025-06-29 21:20:57 -0600

    t1

--- VBox/main.tf
+++ VBox/main.tf
@@ -11,9 +11,9 @@ terraform {
 
 resource "virtualbox_vm" "node" {
   count     = 2
-  name      = "marioVB"
+  name      = "marioVB-Puppy"
   #name      = format("node-%02d", count.index + 1)
-  #image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  image     = "/home/mario/Downloads/puppy.iso"
   cpus      = 2
   memory    = "512 mib"
   #user_data = file("${path.module}/user_data")

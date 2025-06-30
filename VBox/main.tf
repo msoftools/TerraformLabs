c81ebd3a6ad842b95472a99f0e7b822bc425e133
msoftools <msoftools@gmail.com>
2025-06-29 21:05:50 -0600

    t1

--- VBox/main.tf
+++ VBox/main.tf
@@ -11,8 +11,9 @@ terraform {
 
 resource "virtualbox_vm" "node" {
   count     = 2
-  name      = format("node-%02d", count.index + 1)
-  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  name      = "marioVB"
+  #name      = format("node-%02d", count.index + 1)
+  #image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
   cpus      = 2
   memory    = "512 mib"
   #user_data = file("${path.module}/user_data")
@@ -33,3 +34,6 @@ output "IPAddr_2" {
 
 
 #Tutorial: https://www.roksblog.de/terraform-virtualbox-provider-terrafarm/
+
+
+
